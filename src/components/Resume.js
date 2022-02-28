ea8c1d4ebda69345ae0d186e41612909e3ce8e62
Queen McSteve <niko.desilva@esmt.org>
2022-02-28 20:24:17 +0100

    update resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,12 @@ function Resume() {
     <div className="flex flex-col justify-center">
       Download my CV here:
       <br />
-      <button>download</button>
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        ahref="/NdS_CV_January2022.pdf"
+      >
+        download
+      </button>
     </div>
   );
 }
