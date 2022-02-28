f5eef553b000dd40ce1f09aeeb71333428e72e8f
Queen McSteve <niko.desilva@esmt.org>
2022-02-28 20:58:37 +0100

    fix resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,12 +3,12 @@ function Resume() {
     <div className="flex flex-col justify-center">
       Download my CV here:
       <br />
-      <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        ahref="/NdS_CV_January2022.pdf"
+      <a
+        className="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded"
+        href="/NdS_CV_January2022.pdf"
       >
         download
-      </button>
+      </a>
     </div>
   );
 }
